860c23bfd5564f05b884b3fe29b19078b19e1907
Daniel Kupczyk <daniel.kupczyk@siemens-healthineers.com>
2022-01-21 09:05:08 +0100

    Added a healthcheck controller

---
+++ src/homeautomation.service/Controllers/HealthCheckController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Homeautomation.Service.Controllers
+{
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        [Route("healthCheck/version")]
+        public string GetVersion()
+        {
+            System.Reflection.Assembly assembly = typeof(HealthCheckController).Assembly;
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            return fvi.FileVersion;
+        }
+
+        [HttpGet]
+        [Route("healthCheck/ping")]
+        public bool Ping()
+        {
+            return true;
+        }
+    }
+}
